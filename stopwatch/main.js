a69725f5071a0b13899ac1f450516965bfcd4d3a
omincron <patou505@gmail.com>
2021-10-16 19:01:26 +0300

    Update code

--- stopwatch/main.js
+++ stopwatch/main.js
@@ -6,11 +6,11 @@ let start;
 startTimer.onclick = function startTimer() {
   clearInterval(start);
   start = window.setInterval(run, 10);
-}
+};
 
 stopTimer.onclick = function stopTimer() {
   clearInterval(start);
-}
+};
 
 resetTimer.onclick = function reset() {
   clearInterval(start);
@@ -20,44 +20,44 @@ resetTimer.onclick = function reset() {
   document.getElementById("min").innerHTML = "00";
   document.getElementById("sec").innerHTML = "00";
   document.getElementById("ms").innerHTML = "00";
-}
+};
 
 function run() {
-  if (ms <= 99) {
-    if (sec > 59) {
-      minutes();
-      sec = 0;
-    }
-    milisecond();
-  } else if (ms > 99) {
+  milisecond();
+  if (sec == 59) {
+    minutes();
+    sec = 0;
+  }
+  if (ms == 99) {
     seconds();
     ms = 0;
+    console.log(ms);
   }
 }
 
 function minutes() {
+  min += 1;
   if (min <= 9) {
     document.getElementById("min").innerHTML = "0" + min;
   } else {
     document.getElementById("min").innerHTML = min;
   }
-  min += 1;
 }
 
 function seconds() {
+  sec += 1;
   if (sec <= 9) {
     document.getElementById("sec").innerHTML = "0" + sec;
   } else {
     document.getElementById("sec").innerHTML = sec;
   }
-  sec += 1;
 }
 
 function milisecond() {
+  ms++;
   if (ms <= 9) {
     document.getElementById("ms").innerHTML = "0" + ms;
   } else {
     document.getElementById("ms").innerHTML = ms;
   }
-  ms++;
 }
